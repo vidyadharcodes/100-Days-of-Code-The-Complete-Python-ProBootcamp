19401b134e621431ec1c437715fbdf47435c6424
vidyadharcodes <vidyadharpandey010@gmail.com>
2024-09-17 17:37:48 +0530

    This was Day 4

---
+++ Day 4/Lists/task.py
@@ -0,0 +1,19 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+fruits = ["Cherry", "Apple", "Pear","banana","Grapes"]
+print(states_of_america[0])
+
+states_of_america[1] = "Cherry"
+
+states_of_america.append("New Harshand")
+print(states_of_america)
+
+print(fruits.count('Apple'))
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruits.append('Mangoes')
+print(fruits)
+fruits.sort()
+print(fruits)
+fruits.pop()
+print(fruits)
