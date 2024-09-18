19de9b9c8d00d8e3a5301d2678f7acfba635aa2f
vidyadharcodes <166310910+vidyadharcodes@users.noreply.github.com>
2024-09-18 23:01:18 +0530

    Add files via upload
    Day 05 Done.

---
+++ Day 5/Day 5/For Loops/task.py
@@ -0,0 +1,20 @@
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " pie")
+
+# This code will behave very differently
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print("Hello")
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+print("Hello")
+
+
+'''Indentation is very important in Python programming.
+ Every time you see the : symbol used, you need to be careful about the indentation that comes afterwards.
+'''
---
+++ Day 5/Day 5/Password Generator Project/task.py
@@ -0,0 +1,24 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+#easy_version
+import random
+password = ""
+random_letters = ""
+random_symbols = ""
+random_number = ""
+for i in range(nr_letters):
+    random_letter = random.choice(letters)
+    password += random_letter
+for j in range(nr_symbols):
+    random_symbol = random.choice(symbols)
+    password += random_symbol
+for k in range(nr_numbers):
+    random_number = random.choice(numbers)
+    password += random_number
+print(f"Your password is: {password}")
