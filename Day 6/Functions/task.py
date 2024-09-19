8b27d37e6f581c839ee492b2bdb3e1ff05d0cf27
vidyadharcodes <vidyadharpandey010@gmail.com>
2024-09-20 01:53:19 +0530

    This was Day 06

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,10 @@
+
+#Creating the function
+def get_user_name():
+    name = input("What is your name? ")
+    print("Hello, " + name)
+    # Inside the function
+
+#Outside the function
+print("Hello")
+get_user_name() # Calling the function
